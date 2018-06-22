8db93e347e98ef3f27ce6704dd5fc08cb7e72c98
Pedro Cassone <pedrocassone@gmail.com>
2018-06-22 10:45:32 -0300

    Projeto completo em Inglês-USA

---
+++ English-USA/Site/assets/js/l10n.js
@@ -0,0 +1,303 @@
+var _list = {
+	"txt_menu_1":{
+		"pt":"História de Portal Challenge",
+		"en":"History of Portal Challenge",
+		"sp":"Primero",
+		"ru":"Peristróica",
+		"ge":"Gutentack"
+	},
+	"txt_menu_2":{
+		"pt":"Página Inicial",
+		"en":"Home Page",
+		"sp":"Producto"
+	},
+	"txt_menu_3":{
+		"pt":"Quem somos",
+		"en":"About us",
+		"sp":"Quienes somos"
+	},
+	"txt_menu_4":{
+		"pt":"Contato",
+		"en":"Contact"
+	},
+	"txt_menu_2_1":{
+		"pt":"Telefonia",
+		"en":"Telephony"
+	},
+	"txt_menu_2_2":{
+		"pt":"Wireless",
+		"en":"Wireless"
+	},
+	"txt_menu_2_3":{
+		"pt":"Interfonia",
+		"en":"Interphone"
+	},
+	"txt_menu_2_4":{
+		"pt":"Alarmes",
+		"en":"Alarms"
+	},
+	"plh_search_1":{
+		"pt":"Pesquisar...",
+		"en":"Search..."
+	},
+	"ttl_search_1":{
+		"pt":"Pesquisar",
+		"en":"Search"
+	},
+	"ttl_flags_1":{
+		"pt":"Português",
+		"en":"Portuguese"
+	},
+	"ttl_flags_2":{
+		"pt":"Inglês",
+		"en":"English"
+	},
+	"ttl_flags_3":{
+		"pt":"Espanhol",
+		"en":"Spanish"
+	},
+	"txt_footer_1":{
+		"pt":"100% brasileira. Assim é a Intelbras, uma empresa que atua nas áreas de Segurança, Telecom e Redes. Porque conhecer e entender as necessidades dos consumidores é um dos motivos que faz a Intelbras líder em vários segmentos nos quais atua.",
+		"en":"100% Brazilian. So is the Intelbras, a company that operates in the areas of Security, Telecom and Networks. Because you know and understand the needs of consumers is one of the reasons that makes Intelbras leader in several segments in which it operates."
+	},
+	"txt_footer_2":{
+		"pt":"Receba as últimas novidades da Intelbras em seu e-mail",
+		"en":"Receive the Intelbras latest news in your mail box."
+	},
+	"txt_footer_3":{
+		"pt":"Siga-nos em nossas redes sociais",
+		"en":"Follow us in social media"
+	},
+	"plh_footer_1":{
+		"pt":"Digite seu e-mail",
+		"en":"Type your email address"
+	},
+	"val_footer_1":{
+		"pt":"Cadastrar",
+		"en":"Register"
+	},
+	"txt_banner_1_1":{
+		"pt":"Intelbras e você",
+		"en":"Intelbras and you"
+	},
+	"txt_banner_1_2":{
+		"pt":"juntos há 40 anos",
+		"en":"together for 40 years"
+	},
+	"txt_banner_2_1":{
+		"pt":"Modernas instalações",
+		"en":"Advanced installations"
+	},
+	"txt_banner_2_2":{
+		"pt":"ótimos profissionais",
+		"en":"best professionals"
+	},
+	"txt_banner_3_1":{
+		"pt":"Tecnologia de ponta",
+		"en":"State-of-the-art"
+	},
+	"txt_banner_3_2":{
+		"pt":"fabricada no Brasil",
+		"en":"made in Brazil"
+	},
+	"txt_home_1":{
+		"pt":"Encontre um revendedor Intelbras o main próximo de você",
+		"en":"Find an authorized reseller Intelbras closer to you"
+	},
+	"plh_home_1":{
+		"pt":"Digite sua cidade ou CEP",
+		"en":"Enter your city or zip code"
+	},
+	"val_home_1":{
+		"pt":"Pesquisar",
+		"en":"Search"
+	},
+	"txt_home_2":{
+		"pt":"Precisa de ajuda? Experimente nosso Suporte On-line",
+		"en":"Do you need help? Try our On-line Support"
+	},
+	"plh_home_2":{
+		"pt":"Seu nome",
+		"en":"Your name"
+	},
+	"val_home_2":{
+		"pt":"Entrar",
+		"en":"Enter"
+	},
+	"txt_home_3":{
+		"pt":"Nossos Parceiros",
+		"en":"Our associates"
+	},
+	"txt_home_4":{
+		"pt":"Últimas Notícias",
+		"en":"Last news"
+	},
+	"txt_news_1":{
+		"pt":"Conheça o Roteador que libera Wi-Fi com check-in no Facebook",
+		"en":"Meet the router that release Wi-Fi to check-in on Facebook"
+	},
+	"txt_news_2":{
+		"pt":"Intelbras na Equipotel 2015",
+		"en":"Intelbras in Equipotel 2015"
+	},
+	"txt_news_3":{
+		"pt":"Intelbras é destaque no Anuário Telecom 2015",
+		"en":"Intelbras is featured in Telecom 2015"
+	},
+	"txt_news_4":{
+		"pt":"Intelbras promove Encontro Internacional de Vendas",
+		"en":"Intelbras promotes Internacional Sales Meeting"
+	},
+	"txt_category_1":{
+		"pt":"Telefonia",
+		"en":"Telephony"
+	},
+	"txt_category_2":{
+		"pt":"Telefone Sem Fio TS 3110",
+		"en":"Wireless Telephone TS 3110"
+	},
+	"txt_single_1":{
+		"pt":"Telefone Sem Fio TS 3110",
+		"en":"Wireless Telephone TS 3110"
+	},
+	"txt_single_2":{
+		"pt":"Tenha qualidade excepcional durante as ligações com um aparelho moderno e sofisticado!",
+		"en":"Has exceptional quality during calls with a modern and sophisticated handset!"
+	},
+	"txt_single_3":{
+		"pt":"O TS 3110 é um telefone adequado para diversos ambientes e para toda a familia. Tanto em sua casa como no escritório ele pode ser uma ótima opção. Possui formato ergonomico e é recomendado para longas conversas, pois possui bateria de alta duração. Várias funções compõem este aparelho, tais como: Identificador de chamadas, Chamada simultânea, Redial, Chamada em espera, Transferência de chamada e outras.",
+		"en":"The TS 3110 is a appropriate telephone for different ambience and for all your family both at your home and in the office. It has ergonomic shape and is recommended for long conversations, because it has high battery life. Several functions make this equipment, such as: Calls identifier, Simultaneous calls, Redial, Call waiting, Call transfer and others."
+	},
+	"txt_single_4":{
+		"pt":"Fale por mais tempo com a qualidade de ligação que só a Intelbras traz para você!",
+		"en":"Speaks longer with the connection quality that Intelbras only brings to you."
+	},
+	"txt_single_5":{
+		"pt":"Características",
+		"en":"Features"
+	},
+	"txt_single_6":{
+		"pt":"Especificações",
+		"en":"Specifications"
+	},
+	"txt_single_7":{
+		"pt":"O telefone sem fio TS 3110 possui design ergonômico e bateria de alta duração, perfeito para quem utiliza o aparelho por longas horas ou necessita de mobilidade durante as ligações. Capacidade para até 7 ramais (base + 6 ramais)",
+		"en":"The Wireless Telephone TS 3110 has ergonomic shape and is recommended for long conversations, because it has high battery life, perfect for those who use the device for long hours or mobility needs during calls. Capacity for up to 7 branch lines"
+	},
+	"txt_single_8":{
+		"pt":"Ramais compatíveis: TS 3111, TS 40 R e TS 60 R",
+		"en":"Compatible branch lines: TS 3111, TS 40 R e TS 60 R"
+	},
+	"txt_single_9":{
+		"pt":"Tecnologia DECT 6.0",
+		"en":"DECT 6 Technology."
+	},
+	"txt_single_10":{
+		"pt":"Eco Mode para redução do consumo de energia",
+		"en":"Eco Mode to reduce consumption energy"
+	},
+	"txt_single_11":{
+		"pt":"Comunicação interna, conferência a três e transferência de chamadas",
+		"en":"Inside Comunication, Conference up to 3 people and call transfer"
+	},
+	"txt_single_12":{
+		"pt":"Acompanha: 1 fone, 1 base, 1 bateria recarregável, 1 cabo de linha telefônica, 1 adaptador de tensão 110-220 V, 1 manual do usuário",
+		"en":"Itens included: 1 Headset, 1 Base, 1 Rechargeable Batteries, 1 Telephone Line Extension Cable Cord, 1 Power Adaptor 110-220 V, 1 manual"
+	},
+	"txt_single_13":{
+		"pt":"Ramais compativeis: TS 3111, TS 40 R e TS 60 R",
+		"en":"Compatible branch lines: TS 3111, TS 40 R e TS 60 R"
+	},
+	"txt_single_14":{
+		"pt":"Funções realizáveis entre telefones Intelbras das séries TS 31, TS 40 e TS 60",
+		"en":"Functions performed between Intelbras phones of series: TS 31, TS 40 and TS 60"
+	},
+	"txt_about_1":{
+		"pt":"A Empresa",
+		"en":"The Company"
+	},
+	"txt_about_2":{
+		"pt":"Fundada em 1976, a Intelbras é uma empresa brasileira com orgulho, e com uma tecnologia que alcança mais de 15 países para os quais exporta.",
+		"en":"Founded in 1976, Intelbras is proud to be a Brazilian company and have a technology that reaches more than 15 countries where we export to."
+	},
+	"txt_about_3":{
+		"pt":"Atualmente, o grupo Intelbras possui cinco unidades fabris: Matriz, Filial São José SC, Filial Palhoça SC, além das Filiais em Minas Gerais e no Amazonas.",
+		"en":"Currently Intelbras group counts on six manufacturing units: headquarters, facility II, Automatiza and Engesul,all of them in Santa Catarina, besides two branches in Minas Gerais and Amazonas."
+	},
+	"txt_about_4":{
+		"pt":"A Intelbras possui um dos maiores centros de pesquisa e desenvolvimento privados da América Latina, além de uma das maiores redes de assistência técnica do mercado brasileiro.",
+		"en":"Intelbras has one of the largest private research and development centers in Latin America, and one of the largest service networks in the Brazilian market."
+	},
+	"txt_about_5":{
+		"pt":"A Intelbras possui importantes certificações, como a ISO 14001 na matriz, que assegura a realização do controle operacional sobre seus aspectos e impactos ambientais, e a ISO 9001, também na matriz, parque fabril II, Manaus (certificadas pelo BVC) e Filial MG (certificada pelo RINA).",
+		"en":"Intelbras has important certifications as ISO 14001 at headquarters, which ensures the execution of operating control over its environmental aspects and impacts, as well as ISO 9001 at headquarters, industrial facility II, MG and Manaus/AM branches."
+	},
+	"txt_about_6":{
+		"pt":"Com mais de 2 mil colaboradores, a Intelbras se destaca como uma das melhores empresas para se trabalhar no Brasil, conforme pesquisas das revistas Exame, Você S/A e Época.",
+		"en":"With approximately 2,000 employees, Intelbras stands out as one of the best companies to work for in Brazil, as it´s been published on magazines such as Exame, Você S/A and Época."
+	},
+	"txt_about_7":{
+		"pt":"Atuação",
+		"en":"Performance"
+	},
+	"txt_about_8":{
+		"pt":"A Intelbras oferece as mais completas soluções em Segurança, Telecom e Redes, integradas sob a mesma plataforma tecnológica, na qual todas as tecnologias são compatíveis.",
+		"en":"Intelbras Security offers the most complete solutions in Telecom and networks, integrated on the same platform technology, in which all the technologies are compatible."
+	},
+	"txt_about_9":{
+		"pt":"A estrutura da Intelbras possui uma equipe apta a desenvolver projetos personalizados de comunicação para clientes corporativos em todo o Brasil. São soluções feitas para suprir necessidades específicas.",
+		"en":"The structure of Intelbras has a capable team to develop personalized communication projects for corporate clients in the Brazil. Are solutions made to meet specific needs."
+	},
+	"txt_about_10":{
+		"pt":"O que o cliente precisa, a Intelbras oferece, sempre com a qualidade de uma marca líder.",
+		"en":"What the customer needs, Intelbras offers, always with the quality of a leading brand."
+	},
+	"txt_about_11":{
+		"pt":"Para mais informações sobre o atendimento corporativo, entre em contato com a nossa equipe.",
+		"en":"For more information about the corporate service, please contact our team."
+	},
+	"txt_about_12":{
+		"pt":"Responsabilidade social",
+		"en":"Social responsability"
+	},
+	"txt_about_13":{
+		"pt":"A Intelbras considera as ações sociais um fator importante e, por conta disso, utiliza toda a verba autorizada para projetos aprovados pela Lei de Incentivo à Cultura (Rouanet) ou pela Lei de Incentivo à Cultura, Turismo e Esporte de Santa Catarina (ICMS).",
+		"en":"Intelbras considers social action an important factor and, for that reason, uses all the authorized funds to projects approved by the Law on Cultural Incentives (Rouanet) or the Law for the Encouragement of Culture, Tourism and Sports of Santa Catarina (ICMS)."
+	},
+	"txt_about_14":{
+		"pt":"O foco da empresa para os patrocínios são os projetos ligados à responsabilidade social com crianças, menores carentes e o meio ambiente, seja através de música, esporte, prevenção, cinema ou teatro.",
+		"en":"The company's focus for sponsorships are the projects related to social responsibility with children, poor children and the environment, whether through music, sports, prevention, cinema or theater."
+	},
+	"txt_contact_1":{
+		"pt":"Contato",
+		"en":"Contact"
+	},
+	"plh_contact_1":{
+		"pt":"Nome",
+		"en":"Name"
+	},
+	"plh_contact_2":{
+		"pt":"E-mail",
+		"en":"Email"
+	},
+	"txt_contact_2":{
+		"pt":"Escolha um departamento",
+		"en":"Choose a department"
+	},
+	"plh_contact_3":{
+		"pt":"Assunto",
+		"en":"Subject"
+	},
+	"plh_contact_4":{
+		"pt":"Mensagem",
+		"en":"Message"
+	},
+	"val_contact_1":{
+		"pt":"Enviar",
+		"en":"Send"
+	},
+	"txt_contact_3":{
+		"pt":"Localização",
+		"en":"Location"
+	}
+};
